cd391ad529c0dca0f5fdd9e2ec2aecdc08145a55
Vinicius Santos <vinixreduzino@hotmail.com>
2018-05-31 16:51:49 -0300

    Parte 1 Da Myllena

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const config = require('./config.json');
+const Discord = require('discord.js');
 
+bot.on('ready' , async () =>{
+    console.log(`${bot.user.username} is online!`)
+})
+
+bot.login(process.env.BOT_TOKEN);
