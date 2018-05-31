95a81738d10cd7a1a3a7e44ac0e7c5c2603f0794
Vinicius Santos <vinixreduzino@hotmail.com>
2018-05-31 16:54:26 -0300

    Parte 1 da Myllena

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const config = require('./config.json');
 const Discord = require('discord.js');
+const myllena = new Discord.Client({disableEveryone: true})
 
-bot.on('ready' , async () =>{
+
+myllena.on('ready' , async () =>{
     console.log(`${bot.user.username} is online!`)
 })
 
-bot.login(process.env.BOT_TOKEN);
+myllena.login(process.env.BOT_TOKEN);
