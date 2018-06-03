6a8d9be4e4913088389a0bba54ee4290132bacca
Vinicius Santos <vinixreduzino@hotmail.com>
2018-06-03 10:26:09 -0300

    A

--- commands/kick.js
+++ commands/kick.js
@@ -1,21 +1,19 @@
-exports.run =  (client , message , args) =>{
-    const Discord = require('discord.js');
-    //m!kick @user reason
-let PermissaoKickReturn = new Discord.RichEmbed()
-    .setColor('#4b42f4')
-    .setDescription('Você não tem permissao para kickar :oncoming_police_car:')
-let MentionMemberReturn = new Discord.RichEmbed()
-    .setColor('#4b42f4')
-    .setDescription('Uso invalido. Por Favor faça como o seguinte exemplo:`d!kick @user#0000 `')
-let KickForUser = new Discord.RichEmbed()
-    .setColor('#4b42f4')
-    .setDescription(`Kickado por${message.author.tag}`)
+exports.run = (client , message , args) =>{
+    const Discord =require('discord.js')
+var PermissaoKickReturn = new Discord.RichEmbed()
+.setColor('#4b42f4')
+.setDescription('Você não tem permissao para kickar :oncoming_police_car:')
+var MentionMemberReturn = new Discord.RichEmbed()
+.setColor('#4b42f4')
+.setDescription('Uso invalido. Por Favor faça como o seguinte exemplo:`d!kick @user#0000 `')
+var KickForUser = new Discord.RichEmbed()
+.setColor('#4b42f4')
+.setDescription(`Kickado por${message.author.tag}`)
+//Fim do embed
 
-
-    if(!message.member.hasPermission('KICK_MEMBERS', 'ADMINISTRATOR'))//Verifica se o usuario tem essas permissoes return (message.channel.send(PermissaoKickReturn))
-    let PrimeiraMention = message.mentions.members.first()
-    if(!PrimeiraMention)return (message.channel.send(MentionMemberReturn))
-    PrimeiraMention.kick(KickForUser);
-    return;
-    
+if(!message.member.hasPermission('KICK_MEMBERS', 'ADMINISTRATOR'))/*Verifica se o usuario tem essas permissoes*/ return (message.channel.send(PermissaoKickReturn))
+        const PrimeiraMention = message.mentions.members.first()
+if(!PrimeiraMention)return (message.channel.send(MentionMemberReturn))
+PrimeiraMention.kick(KickForUser);
 }
+
