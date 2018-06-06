5443652bcaf40f8f06a3fade2d603b09936043c8
Vinicius Santos <vinixreduzino@hotmail.com>
2018-06-06 19:01:44 -0300

    a

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,6 @@
 exports.run = (client , message ,  args) =>{
     const Discord = require('discord.js');
-    if(!message.member.hasPermission("ADMINISTRATOR")) return;
+    if(!message.member.hasPermission("SEND_MESSAGES")) return mesasge.reply("nop");
          const sayMessage = args.join(" ");
          message.delete().catch();
          message.channel.send(sayMessage);
