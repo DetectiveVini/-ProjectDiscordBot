71ea283bf5ad608ac8ad8e1153227488bc51dfd3
Vinicius Santos <vinixreduzino@hotmail.com>
2018-06-06 18:52:20 -0300

    a

--- commands/help.js
+++ commands/help.js
@@ -2,23 +2,14 @@ exports.run = (client  , message  , args) =>{
     const Discord = require('discord.js');
     const config = require('../config.json')
     const red =  config.vermelho;
-
+    let ComandosDeAdminitração = ['´´´Ban,Kick,Report,Clear,RemoveRole,AddRole´´']
+    let ComandosComuns = ['´´Avatar,BotInfo,Help,Invite,Ping,Say,ServerInfo']
     let HelpEmbed = new Discord.RichEmbed()
         .setColor(red)
         .setDescription('Comandos do Bot')
-        .addField('Addrole')
-        .addField('avatar')
-        .addField('ban')
-        .addField('botinfo')
-        .addField('clear')
-        .addField('help')
-        .addField('invite')
-        .addField('kick')
-        .addField('ping')
-        .addField('removerole')
-        .addField('report')
-        .addField('say')
-        .addField('serverinfo')
+        .addField('Comandos de Administração', ComandosDeAdminitração)
+        .addField('Comandos Comuns' , ComandosComuns);
+
 
 
     return message.channel.sendMessage(HelpEmbed);
