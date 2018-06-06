a8b6ff2bffe5ea5b591b761a298c230aaa4edbe9
Vinicius Santos <vinixreduzino@hotmail.com>
2018-06-06 17:49:07 -0300

    a

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js')
 exports.run = (client , message , args) =>{
     const Discord = require('discord.js');
-    let bicon = myllena.user.displayAvatarURL;
+    let bicon = client.user.displayAvatarURL;
     let botinfo = new Discord.RichEmbed()
         .setDescription("**BOT INFO**")
         .setColor("#42f450")
         .setThumbnail(bicon)
-        .addField("Bot Name" , myllena.user.username)
-        .addField("Criado Em" , myllena.user.createdAt)
+        .addField("Bot Name" , client.user.username)
+        .addField("Criado Em" , client.user.createdAt)
+        
     return message.channel.sendMessage(botinfo);
 }
---
+++ commands/help.js
@@ -0,0 +1,25 @@
+exports.run = (client  , message  , args) =>{
+    const Discord = require('discord.js');
+    const config = require('../config.json')
+    const red =  config.vermelho;
+
+    let HelpEmbed = new Discord.RichEmbed()
+        .setColor(red)
+        .setDescription('Comandos do Bot')
+        .addField()
+        .addField()
+        .addField()
+        .addField()
+        .addField()
+        .addField()
+        .addField()
+        .addField()
+        .addField()
+        .addField()
+        .addField()
+
+
+
+
+
+}
