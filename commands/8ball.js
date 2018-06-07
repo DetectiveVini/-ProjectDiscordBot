afc4242098b08345de44dce2eec78877279f5611
Vinicius Santos <vinixreduzino@hotmail.com>
2018-06-07 17:13:40 -0300

    a

--- commands/8ball.js
+++ commands/8ball.js
@@ -6,13 +6,13 @@ exports.run = async (client , message , args) =>{
     //m!8ball <pergunta  etc>
     //args[1] = pergunta
     //args[0] = 8ball
-    //args[2] = etc
+    
 
-    if(!args[2]) return message.channel.sendMessage('Pergunte alguma coisa!');
+    if(!args[1]) return message.channel.sendMessage('Pergunte alguma coisa!');
     let respostas = ["Sim" , "Não" , "Não sei" , "Pergunte Denovo"];
 
     let resuntado = Math.floor((Math.random()* respostas.length/*Vai multiplicar pela quantidade de respostas*/));
-    let pergunta = args.slice(1).join(" ");
+    let pergunta = args.slice(0).join(" ");
 
     let BallEmbed = new Discord.RichEmbed()
         .setAuthor(message.author.tag)//Nick do autor da message ou comando
