e67bca86a7ab95b153e88ea5589db8a7e9c9098c
Vinicius Santos <vinixreduzino@hotmail.com>
2018-06-07 17:41:13 -0300

    a

--- commands/help.js
+++ commands/help.js
@@ -2,17 +2,19 @@ exports.run = (client  , message  , args) =>{
     const Discord = require('discord.js');
     const config = require('../config.json')
     const red =  config.vermelho;
-    let ComandosDeAdminitração = ['**Ban,Kick,Report,Clear,RemoveRole,AddRole**']
-    let ComandosComuns = ['**Avatar,BotInfo,Help,Invite,Ping,Say,ServerInfo**']
+    let ComandosDeAdminitração = ['**Ban,Kick,Report,Clear,RemoveRole,AddRole**'];
+    let ComandosComuns = ['**Avatar,BotInfo,Help,Invite,Ping,Say,ServerInfo**'];
+    let ServidoresParceiros = ['[Avengers Without Souls](https://discord.gg/MnaC3hJ)','[Ayumi - Discord Server!](https://discord.gg/Z2Bf4nB)'];
+    let Github = ['[Myllena](https://github.com/DetectiveVini/Myllena)'];
+    
+    
     let HelpEmbed = new Discord.RichEmbed()
         .setColor(red)
         .setDescription('Comandos do Bot')
         .addField('Comandos de Administração', ComandosDeAdminitração)
         .addField('Comandos Comuns' , ComandosComuns)
-        .addField('Servidores Parceiros' ,'[Avengers Without Souls](https://discord.gg/MnaC3hJ)','[Ayumi - Discord Server!](https://discord.gg/Z2Bf4nB)')
-        .addField('Repositorio Github', '[Myllena](https://github.com/DetectiveVini/Myllena)');
-
-
+        .addField('Servidores Parceiros' , ServidoresParceiros) 
+        .addField('Github' , Github);
 
     return message.channel.sendMessage(HelpEmbed);
 
