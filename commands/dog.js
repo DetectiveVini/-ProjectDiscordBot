b21113b7f99844cf1b502b7bd9e823f855b38c1d
Vinicius Santos <vinixreduzino@hotmail.com>
2018-06-07 16:17:48 -0300

    a

--- commands/dog.js
+++ commands/dog.js
@@ -1,10 +1,10 @@
-exports.run = (client ,message , args) =>{
+exports.run = async (client ,message , args) =>{
     const Discord = require('discord.js');
     const superagent = require('superagent');
     const config = require('../config.json');
     const roxo = config.roxo;
 
-    let {body} = superagent
+    let {body} = await superagent
         .get(`https://random.dog/woof.json`);
     
     let DogEmbed = new Discord.RichEmbed()
