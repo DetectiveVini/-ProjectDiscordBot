5a2979af5a46cb104c22c51db651b325f69f3cbc
Vinicius Santos <vinixreduzino@hotmail.com>
2018-06-10 17:51:44 -0300

    a

--- commands/help.js
+++ commands/help.js
@@ -9,22 +9,8 @@ exports.run = (client  , message  , args) =>{
     let ServidoresParceiros = ['[Avengers Without Souls](https://discord.gg/MnaC3hJ)','[Ayumi - Discord Server!](https://discord.gg/Z2Bf4nB)'];
     let Github = ['[Myllena](https://github.com/DetectiveVini/Myllena)'];
     
-    //m!help comando
-//comando  = args[1]
 
 
-        if(args[1] === 'ban'){
-            message.channel.sendMessage(HelpBanEmbed)
-        }
-    let HelpBanEmbed = new Discord.RichEmbed()
-    .setColor(red)
-    .setDescription('a')
-    .addField()
-    .addField()
-    .addField() 
-    .addField();
-
-    
 
     let HelpEmbed = new Discord.RichEmbed()
         .setColor(red)
