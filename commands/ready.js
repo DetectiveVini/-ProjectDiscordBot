0049ee45dfc70e2c34bbeb7ef662a4c00aa02fce
Vinicius <vinixreduzino@hotmail.com>
2018-06-23 17:39:03 -0300

    Ready

--- commands/ready.js
+++ commands/ready.js
@@ -8,7 +8,7 @@ module.exports.run = async(client , message , args) =>{
     
     if(message.author.id !==  OwnerID) return message.channel.sendMessage('Você não pode usar este comando');
     
-    client.user.setPresence({ game: { name: `${ModificandoOStatus}`, type:  1} });
+    client.user.setPresence({ game: { name: `${args[0]}`, type:  1} });
 
         const ModificandoOStatus  = args.join(" ");
         let NomeDoUsuario = (message.author.tag)
